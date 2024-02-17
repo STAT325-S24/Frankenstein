ef95f87605226b310b47615c381777e1923542c8
jpapagelis24 <jpapagelis24@amherst.edu>
2024-02-16 21:12:22 -0500

    Worked on the Frankenstein package

--- R/data.R
+++ R/data.R
@@ -3,12 +3,14 @@
 #' This is more descriptive text about my data.
 #' Even more info is provided here ...
 #'
-#' @format ## `foo`
-#' A data frame with XX rows and XX columns:
+#' @format ## `Frankenstein`
+#' A data frame with 7357 rows and 4 columns:
 #' \describe{
-#'   \item{lines}{lines of text}
-#'   \item{author}{who wrote that line}
+#'   \item{line}{line number}
+#'   \item{text}{the text}
+#'   \item{section}{the section number}
+#'   \item{section_type}{the type of section: letter or chapter}
 #'   ...
 #' }
-#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
-"foo"
+#' @source <https://www.gutenberg.org/files/84/84-h/84-h.htm>
+"Frankenstein"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,9 +1,44 @@
 ## code to prepare `DATASET` dataset goes here
 
-library(tidyverse)
-foo <- tibble(
-  lines = c("this is a test", "this is only a test"),
-  author = c("me", "you")
+#install.packages("gutenbergr")
+library(gutenbergr)
+library(dplyr)
+library(tidytext)
+library(stringr)
+
+my_mirror <- "http://mirror.csclub.uwaterloo.ca/gutenberg/"
+frankenstein_raw <- gutenberg_download(84, mirror = my_mirror)
+
+Frankenstein <- frankenstein_raw |>
+  mutate(line = row_number())
+
+Frankenstein$section_type <- NA
+Frankenstein$section <- NA
+num_lines <- nrow(Frankenstein)
+curr_section <- NA
+type <- NA
+for (i in 1:num_lines) {
+  if (str_detect(Frankenstein$text[i], "^Letter")) {
+    curr_section <- as.numeric(str_replace_all(Frankenstein$text[i], "\\D", ""))
+    type <- "letter"
+  }
+  
+  if (str_detect(Frankenstein$text[i], "^Chapter")) {
+    curr_section <- as.numeric(str_replace_all(Frankenstein$text[i], "\\D", ""))
+    type <- "chapter"
+  }
+  
+  Frankenstein$section[i] <- curr_section
+  Frankenstein$section_type[i] <- type
+  
+}
+
+
+Frankenstein <- tibble(line = Frankenstein$line, 
+                       text = Frankenstein$text, 
+                       section = Frankenstein$section,
+                       section_type = Frankenstein$section_type
 )
-usethis::use_data(foo, overwrite = TRUE)
+usethis::use_data(Frankenstein, overwrite = TRUE)
+
 
