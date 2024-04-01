8b5a4a556e7305aff0269d92b576033aa1920447
jpapagelis24 <jpapagelis24@amherst.edu>
2024-04-01 13:43:22 -0400

    Fixed documentation

--- R/data.R
+++ R/data.R
@@ -14,4 +14,20 @@
 #' }
 #' @source <https://www.gutenberg.org/files/84/84-h/84-h.htm>
 "Frankenstein"
+#'
+#' Annotated Frankenstein Data Frame
+#' This data frame provides the annotated text of Frankenstein. Using token shows each token and part of speech with other variables. Using enity shows the specific entities such as countries or names. Document shows each document that the text is split into.
+#'
+#' @format ## `anno_Frankenstein`
+#' 3 elements
+#' \describe{
+#'   \item{token}{A tibble with 85810 rows and 10 columns}
+#'   \item{entity}{A tibble with 1643 rows anf 6 columns}
+#'   \item{document}{A tibble with 6387 rows and 1 column}
+#'   ...
+#' }
+#' @source annotate using cleanNLP on Frankestein text
+"anno_Frankenstein"
+
+
 
